8f5807076ac681b2fa9ed8a0dde03789c8264df8
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-03-30 17:16:39 +0000

    First test and readme

---
+++ index.js
@@ -0,0 +1,32 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const commands = [
+  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+]
+  .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
+
+//Following video instructions to make the bot work on the test server
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(process.env.TOKEN);
